655a4c3d506165d470cbd174b9e7c56013df5bb2
王磊 <arley90@wangleideMacBook-Air.local>
2017-10-18 14:47:51 +0800

    北京3456家，上海699家，江苏899家，安徽495家

---
+++ urls_beijing.js
@@ -0,0 +1,28 @@
+//此项目抽取数据库中已入库所有书店url
+
+var MongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/bookstore';
+ 
+var selectData = function(db, callback) {  
+    //连接到表  
+    var collection = db.collection('beijing');
+    //查询数据
+    var whereStr = {};  //关键查询语句，为空表示拿到集合全部元素
+    collection.find(whereStr).toArray(function(err, result) {
+      if(err)
+      {
+        console.log('Error:'+ err);
+        return;
+      }     
+      callback(result);
+    });
+  }
+   
+  MongoClient.connect(DB_CONN_STR, function(err, db) {
+    console.log("连接成功！");
+    selectData(db, function(result) {
+      for(var i in result){
+      console.log(result[i].href); }   //在此处使用循环
+      db.close();
+    });
+  });
